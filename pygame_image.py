c607d41ce7db89a61fe6e32f60a0c3d3bdc38c80
okadareo <c0a2203640@edu.teu.ac.jp>
2023-06-13 16:27:51 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -3,28 +3,45 @@ import pygame as pg
 
 
 def main():
-    pg.display.set_caption("はばたけ!こうかとん")
+    pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
-    clock  = pg.time.Clock()
-    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    kk_img= pg.image.load("ex01/fig/3.png") 
-    kk_img= pg.transform.flip(kk_img, True, False) 
-    kk_img= pg.transform.rotozoom(kk_img, 10, 1.0)
-    tmr = 0
+    clock = pg.time.Clock()
 
+    # 背景画像をロード
+    bg_img1 = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img2 = pg.image.load("ex01/fig/pg_bg.jpg")
+
+    # 背景画像の横座標の初期値
+    x = 0
+
+    # 3.pngを読み込み、Surfaceを生成
+    kk_img = pg.image.load("ex01/fig/3.png")
+    # 左右を反転
+    kk_img = pg.transform.flip(kk_img, True, False)
+
+    # 画像Surfaceを要素とするリストを生成
     image_list = [kk_img, pg.transform.rotate(kk_img, 10)]
 
+    tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
+            if event.type == pg.QUIT:
+                return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img1, [x, 0])
+        screen.blit(bg_img2, [x - 1600, 0])
+
+        # 画像Surfaceを交互に表示
         image_index = tmr % len(image_list)
         image = image_list[image_index]
         screen.blit(image, (300, 200))
+
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1
+        clock.tick(100)
+
+        # 背景画像の横座標を更新
+        x = (x - 1) % 1600
 
 
 if __name__ == "__main__":
